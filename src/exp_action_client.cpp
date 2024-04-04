44b33ed67e1001d79f9f2c49bb8e9f319ebb0a12
Andy-Leo10 <andres.alamo@pucp.edu.pe>
2024-04-04 09:15:31 -0500

    Action is working as expected!!

--- src/exp_action_client.cpp
+++ src/exp_action_client.cpp
@@ -10,7 +10,8 @@ using GoalHandleWaypoint = rclcpp_action::ClientGoalHandle<WaypointAction>;
 class WaypointActionClient : public rclcpp::Node
 {
 public:
-    WaypointActionClient() : Node("waypoint_action_client")
+    WaypointActionClient() : Node("waypoint_action_client"),
+                            goal_done_(false)
     {
         this->client_ptr_ = rclcpp_action::create_client<WaypointAction>(
             this,
@@ -24,6 +25,8 @@ public:
 
     std::shared_future<GoalHandleWaypoint::SharedPtr> send_goal(double x, double y, double z)
     {
+        this->goal_done_ = false;
+
         auto goal_msg = WaypointAction::Goal();
         goal_msg.position.x = x;
         goal_msg.position.y = y;
@@ -34,12 +37,19 @@ public:
             std::bind(&WaypointActionClient::goal_response_callback, this, std::placeholders::_1);
         send_goal_options.feedback_callback =
             std::bind(&WaypointActionClient::feedback_callback, this, std::placeholders::_1, std::placeholders::_2);
-
+        send_goal_options.result_callback =
+            std::bind(&WaypointActionClient::result_callback, this, std::placeholders::_1);
         return this->client_ptr_->async_send_goal(goal_msg, send_goal_options);
     }
 
+    bool is_goal_done() const
+    {
+        return this->goal_done_;
+    }
+    
 private:
     rclcpp_action::Client<WaypointAction>::SharedPtr client_ptr_;
+    bool goal_done_;
 
     void goal_response_callback(std::shared_future<GoalHandleWaypoint::SharedPtr> future)
     {
@@ -57,6 +67,26 @@ private:
     {
         RCLCPP_INFO(this->get_logger(), "Received feedback: %s", feedback->state.c_str());
     }
+
+    void result_callback(const GoalHandleWaypoint::WrappedResult & result)
+    {
+        switch(result.code)
+        {
+            case rclcpp_action::ResultCode::SUCCEEDED:
+                RCLCPP_INFO(this->get_logger(), "Goal succeeded");
+                this->goal_done_ = true;
+                break;
+            case rclcpp_action::ResultCode::ABORTED:
+                RCLCPP_INFO(this->get_logger(), "Goal was aborted");
+                break;
+            case rclcpp_action::ResultCode::CANCELED:
+                RCLCPP_INFO(this->get_logger(), "Goal was canceled");
+                break;
+            default:
+                RCLCPP_INFO(this->get_logger(), "Unknown result code");
+                break;
+        }
+    }
 };
 
 int main(int argc, char ** argv)
@@ -71,9 +101,14 @@ int main(int argc, char ** argv)
 
     // Send two goals
     action_client->send_goal(0.5, 0.0, 0.0);
+    while(!action_client->is_goal_done()) {
+        rclcpp::spin_some(action_client);
+    }
     action_client->send_goal(0.0, 0.5, 0.0);
+    while(!action_client->is_goal_done()) {
+        rclcpp::spin_some(action_client);
+    }
 
-    rclcpp::spin(action_client);
     rclcpp::shutdown();
     return 0;
 }
